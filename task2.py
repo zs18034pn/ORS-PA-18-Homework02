1763667e3aff2f1685cd16047ddc1c38c538d9cb
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-02-22 22:43:40 +0100

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,17 @@
+"""
+===================   TASK 2   ====================
+* Name: Convert Kilometers To Miles
+*
+* Write a script that will convert kilometers to
+* miles. Script should ask user for input and check
+* if user input is valid. If not, the script should
+* quit immediately with appropriate message.
+*
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Note: You may use `can_string_be_float` function
+* from previous task to validate user input.
+===================================================
+"""
